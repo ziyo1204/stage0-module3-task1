33fe702f182048ba550abe28818ef8087d8eef4f
ZiyoMuhammad <ziyo1226@gmail.com>
2022-11-28 14:39:38 +0500

    stage0-module3-task1

--- src/main/java/lang/print/gaps/task1/UpdatedApp.java
+++ src/main/java/lang/print/gaps/task1/UpdatedApp.java
@@ -3,6 +3,5 @@ package lang.print.gaps.task1;
 public class UpdatedApp {
     public static void main(String[] args) {
         System.out.println("I'm updated\ncan be compiled. And can write to console");
-        System.out.println("fixMe");
     }
 }
